402cc8c32ef53c3b398fb10881ce053dd152c56d
Mohammad Hammad <m.hammad@gmx.net>
2025-10-25 16:31:55 +0200

    Erste richtige RestApi Anfrage erfolgreich gemacht. Es kann nun ein Employee hinzugefügt werden.Dies wurde mit Postman getestet.

---
+++ backend/src/main/java/com/momo/backend/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.momo.backend.controller;
+
+import com.momo.backend.dto.EmployeeDto;
+import com.momo.backend.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    // Build and Employee Rest Api
+    @PostMapping 
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
