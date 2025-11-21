1f1a8f4f662a955f575aaf2c11e9383f0619c3dc
Mohammad Hammad <m.hammad@gmx.net>
2025-11-21 20:42:40 +0100

    Folder Structur and react native Paper installed

---
+++ frontend/src/types/resources.ts
@@ -0,0 +1,26 @@
+export type User = Employee | Manager;
+
+export type BaseUser = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: UserRole;
+};
+
+export type Employee = BaseUser & {
+  role: UserRole.EMPLOYEE;
+  hourlyRate: number;
+  availability: boolean;
+  managerId: string;
+};
+
+export type Manager = BaseUser & {
+  role: UserRole.MANAGER;
+  employees: Employee[];
+};
+
+export enum UserRole {
+  EMPLOYEE = "EMPLOYEE",
+  MANAGER = "MANAGER",
+}
